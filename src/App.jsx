b868d820bbd97e68144c7aa8d79b9d84f05007e0
Subash-Saajan <subashsaajan@gmil.com>
2025-09-13 10:59:17 +0530

    correct cta

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
         <PortfolioSection />
         <Testimonials />
         <HospitalityBanner />
-        <CTA />
+        <Book />
       </main>
       <Footer />
     </div>
--- src/components/Hospitality.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Hospitality() {
-  return <h1>Ready to Elevate Your Hospitality Experience?</h1>;
-}
-
-export default Hospitality;
